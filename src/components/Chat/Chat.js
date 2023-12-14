fa68692def9f98cdd14025cd83e5b02423b4555e
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-14 18:03:33 +0700

    add readme.md

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ChatIcon from "./ChatIcon";
 import ChatScreen from "./ChatScreen";
 import { useSelector } from "react-redux";
