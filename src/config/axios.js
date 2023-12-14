10e101a2065ebce66a3f46efa322ba7d32fc5551
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-14 09:00:48 +0700

    change url be

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://tungstore.onrender.com/api/',
-    baseURL: 'http://localhost:5000/',
+    // baseURL: 'http://localhost:5000/',
+    baseURL: 'https://tungstore.onrender.com/',
     withCredentials: true
 });
 
